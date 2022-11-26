acaca1060423bb6307109d40c9c01f03cad4d552
Kanishka-08 <111690574+Kanishka-08@users.noreply.github.com>
2022-11-26 13:55:52 +0530

    Python project 1

---
+++ Python Project.py
@@ -0,0 +1,33 @@
+
+
+
+import random
+def prime(x):
+    for i in range(2,x):
+        if(x%i==0):
+            return False
+    return True
+
+a=int(input("Enter the Lower limit: "))
+b=int(input("Enter the Upper limit: "))
+x=random.randint(a,b)
+print("\n")
+print("Range is (%d,%d) and randomly picked number is %d"%(a,b,x))
+if(x%2==0):
+    print("%d is an 'Even' number"%(x))
+else:
+    print("%d is an 'Odd' number"%x)
+if(x==1):
+    print("1 is neither pime nor composite number")
+elif(prime(x)):
+    print("%d is a 'Prime' number"%x)
+else:
+    print("%d is a 'Composite' number"%x)
+    
+
+
+
+
+
+
+
